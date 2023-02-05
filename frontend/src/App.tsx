b6cf765fe90a84111631cb7604df08bb40328bef
William Suryawidjaja <rafa3l389@gmail.com>
2023-02-05 11:44:22 -0800

    chore: added comment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   const [greeting, setGreeting] = useState();
   
   useEffect(() => {
+     // If you are testing locally, rename to port 3000. Eg: fetch('http://127.0.0.1:3000/api/hello')
     fetch('http://127.0.0.1:8080/api/hello')
       .then(res => res.json())
       .then(greeting => setGreeting(greeting.message))
