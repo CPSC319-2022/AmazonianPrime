05b1e88fb6159cae3d265edbcd1e2daa1fc0f430
Josh Luong <joshualuong@hotmail.com>
2023-02-10 23:02:05 -0800

    fix: add user to welcome

--- frontend/src/components/landing-page/WelcomeContent.tsx
+++ frontend/src/components/landing-page/WelcomeContent.tsx
@@ -1,17 +1,18 @@
 import { Button } from "@mui/material";
+import { useAppSelector } from "../../redux/store";
 import "./WelcomeContent.scss";
 
 function WelcomeContent() {
-  // TODO: change the name
+  const user = useAppSelector((state) => state.user.value);
   return (
     <div className="welcome-content">
       <div className="welcome-content__messages-background">
         <div className="welcome-content__messages">
           <div className="welcome-content__main-message">
             <span>
-              At <p>Amazonian Prime</p>,<br/> We're All Prime Members.</span>
+              At <p>Amazonian Prime</p>,<br/> Amazonians Are All Prime Members.</span>
           </div>
-          <span>Welcome John, to Your Office-Powered Marketplace. </span>
+          <span>Welcome {user?.firstName}, to Your Office-Powered Marketplace. </span>
           <Button
             color="secondary"
             variant="contained"
