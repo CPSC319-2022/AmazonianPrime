140308eecdf240cac01bb8c2d70d6c65f086077a
Josh Luong <joshualuong@hotmail.com>
2023-02-13 11:09:54 -0800

    feat: initial work for login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import { Route, useParams, Routes } from 'react-router-dom';
 import LandingPage from './components/landing-page/LandingPage';
 import { ThemeProvider } from '@mui/material/styles';
 import Theme from './ThemeOverrides';
-import { useAppDispatch } from './redux/store';
+import { useAppDispatch, useAppSelector } from './redux/store';
+import { useLocation } from "react-router-dom";
 import NavBar from './components/common/NavBar';
 import { useEffect } from 'react';
 import { useGetUserQuery } from './redux/api/user';
 import { setUser } from './redux/reducers/userSlice';
+import LoginPage from './components/login/LoginPage';
 
 // TODO: move this
 function ProductDetailsPage() {
@@ -18,6 +20,12 @@ function ProductDetailsPage() {
 
 const AppWrapper = () => {
   const dispatch = useAppDispatch();
+  const user = useAppSelector((state) => state.user.value);
+  // TODO: remove this... this is temp work, but we should use logic to see if
+  // user is logged in or not.
+  const location = useLocation();
+  const isOnLoginPage = location.pathname.includes("/login");
+  // end TODO
   const { data } = useGetUserQuery();
   useEffect(() => {
     if (data) {
@@ -27,8 +35,12 @@ const AppWrapper = () => {
 
   return (
     <ThemeProvider theme={Theme}>
-      <NavBar />
+      {
+        // TODO: remove this logic
+      }
+      {!isOnLoginPage && <NavBar />}
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<LandingPage />} />
         <Route path="/listing/:listingId" element={<ProductDetailsPage />} />
       </Routes>
---
+++ frontend/src/components/login/LoginPage.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import './LoginPage.scss';
+
+function LoginPage () {
+    return <div className="login-page">
+        <div className="login-page__content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget orci et enim aliquam feugiat. Praesent id turpis lacinia, blandit felis elementum, eleifend est. Pellentesque mollis tempus turpis, a maximus lectus. Phasellus vel augue a quam volutpat posuere. Suspendisse bibendum velit et libero feugiat finibus. Sed molestie, tellus eu accumsan venenatis, ante orci blandit lacus, eu consectetur leo est ac augue. Phasellus enim diam, varius sed interdum ac, condimentum a orci. Sed luctus pellentesque mauris, eget faucibus dui consectetur vitae. Proin commodo vel turpis non finibus. Vivamus sodales accumsan dapibus. Quisque vehicula tellus enim, sed iaculis eros viverra ut. Nulla dignissim varius bibendum. Nullam lacinia rhoncus dignissim. Morbi tempor magna vitae vulputate scelerisque. Donec dignissim dapibus tellus, ut fermentum ipsum dignissim ac.
+
+Nam metus elit, euismod sed massa at, tincidunt placerat velit. Donec ut vehicula leo. Sed pellentesque sapien at varius maximus. Curabitur dapibus nulla eu ultrices dictum. Sed iaculis efficitur cursus. Morbi eu nisl pharetra ipsum vehicula semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis suscipit convallis facilisis. Ut euismod elementum augue, feugiat vulputate erat lobortis a. Nullam eu dignissim purus.
+
+Donec euismod tincidunt dolor, at aliquam nulla sodales sed. Maecenas ut elit suscipit, pretium magna eget, ornare tellus. Vivamus lacinia nisl vel ante mollis, ac sagittis mauris lacinia. Vivamus ut enim a neque rutrum pellentesque nec nec diam. Nullam venenatis semper libero eu ultricies. Nullam iaculis massa magna. Duis finibus finibus justo, in placerat nunc facilisis quis. Quisque tincidunt elementum enim ut gravida.
+
+Suspendisse at enim ut risus porttitor luctus. Duis pretium eros dui, in sollicitudin nisi varius sed. Aliquam id auctor mauris. Pellentesque velit orci, varius vel hendrerit nec, condimentum et ipsum. Ut ut tempor arcu. Pellentesque eu felis at justo ultrices porta. Sed aliquet mauris quis dictum laoreet. Quisque dui urna, ullamcorper at magna dignissim, semper fermentum risus. Pellentesque sapien lacus, viverra sed iaculis in, consectetur id velit. Suspendisse tempus turpis arcu, et accumsan ante malesuada vel. Aenean pellentesque mollis elit, eget condimentum turpis gravida quis. In hac habitasse platea dictumst. Nunc laoreet dui ac mattis lacinia. Etiam cursus sem nec ligula hendrerit dignissim. Nulla id laoreet ante. Vivamus elementum lacus ac lacus maximus, et imperdiet lectus pharetra.
+
+Nulla posuere laoreet ante, at sollicitudin nisi gravida eu. Nam pharetra dolor e</div>
+    </div>
+}
+
+export default LoginPage;
