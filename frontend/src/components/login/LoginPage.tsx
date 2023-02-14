b33f59ccec44a7e767e0affd7cd694851017e53b
Michael He <michaelhe@Michaels-MacBook-Pro.local>
2023-02-14 05:21:16 -0800

    feat(login-page): temporary google sign in button

--- frontend/src/components/login/LoginPage.tsx
+++ frontend/src/components/login/LoginPage.tsx
@@ -4,6 +4,9 @@ import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
 import './LoginPage.scss';
 import Banner from '../common/Banner';
+import { Button } from '@mui/material';
+import GoogleIcon from '@mui/icons-material/Google';
+import { blue } from '@mui/material/colors'
 
 
 function LoginPage () {
@@ -14,20 +17,23 @@ function LoginPage () {
             <div className="login-page__messages">
                 <div className="login-page__welcome-message">
                     <span>
-                    Welcome to<br /> Amazonian Prime
+                        Welcome to<br /> Amazonian Prime
                     </span>
                 </div>
                     <div className="login-page__mission-statement">
                         <span>
-                        Our goal is to increase<br /><p>interaction</p> and<br /><p>collaboration</p> through<br />
-                        buying and selling items<br />within the Amazonian<br />community
+                            Our goal is to increase<br /><p>interaction</p> and<br /><p>collaboration</p> through<br />
+                            buying and selling items<br />within the Amazonian<br />community
                         </span>
                     </div>
                 </div>
                 <div className="login-page__login-prompts">
-                        <span>
+                    <span>
                         Sign in or Register
-                        </span>
+                    </span>
+                    <Button variant="contained" startIcon={<GoogleIcon sx={{ color: blue[400] }} />} className="login-page__sign-in">
+                        Sign in with Google
+                    </Button>
                 </div>
             </div>
         </div>
