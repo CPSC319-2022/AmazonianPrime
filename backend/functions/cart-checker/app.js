0631764375a297dcbeb66f7a50b1dff3b2a27c17
William Suryawidjaja <rafa3l389@gmail.com>
2023-02-14 00:18:56 -0800

    feat: Setup basic step function using AWS SAM (#19)
    * chore: moved Lambdas to folder
    
    * feat: Setup basic step function
    
    * chore: removed package locks

---
+++ backend/functions/cart-checker/app.js
@@ -0,0 +1,21 @@
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+exports.lambdaHandler = async (event, context) => {
+  // Check current price of the stock
+  random_number = getRandomInt(100); // Current stock price is mocked as a random integer between 0 and 100
+  response = {
+    statusCode: 200,
+    body: "Your number is: " + random_number,
+  };
+  return response;
+};
