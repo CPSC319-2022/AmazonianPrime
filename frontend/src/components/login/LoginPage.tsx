34f29e2ac2515d1c7382ff5fde9f518fe3750051
Michael He <michaelhe@Michaels-MacBook-Pro.local>
2023-02-14 04:10:39 -0800

    feat(login-page): text

--- frontend/src/components/login/LoginPage.tsx
+++ frontend/src/components/login/LoginPage.tsx
@@ -9,9 +9,27 @@ import Banner from '../common/Banner';
 function LoginPage () {
     return (
         <div className="login-page">
-        <div className="user-info-bar">
-            <Banner />
-        </div>
+        <Banner />
+        <div className="login-page__contents">
+            <div className="login-page__messages">
+                <div className="login-page__welcome-message">
+                    <span>
+                    Welcome to<br /> Amazonian Prime
+                    </span>
+                </div>
+                    <div className="login-page__mission-statement">
+                        <span>
+                        Our goal is to increase<br /><p>interaction</p> and<br /><p>collaboration</p> through<br />
+                        buying and selling items<br />within the Amazonian<br />community
+                        </span>
+                    </div>
+                </div>
+                <div className="login-page__login-prompts">
+                        <span>
+                        Sign in or Register
+                        </span>
+                </div>
+            </div>
         </div>
     );
 }
