89e7f7ee421257d37591695b7a493b3f753ef088
Michael He <michaelhe@Michaels-MacBook-Pro.local>
2023-02-14 02:16:16 -0800

    feat(Banner): added logic to work for login flows

--- frontend/src/components/common/Banner.tsx
+++ frontend/src/components/common/Banner.tsx
@@ -1,10 +1,22 @@
 import PinDropIcon from '@mui/icons-material/PinDrop';
 import './Banner.scss';
+import { useLocation } from "react-router-dom";
 import { useAppSelector } from '../../redux/store';
 
 function Banner() {
   const user = useAppSelector((state) => state.user.value);
-  if (!user) return null;
+  const isOnLoginPage = useLocation().pathname.includes("/login");
+
+  if (isOnLoginPage) return (
+    <div className="banner">
+      <span className="banner__content">Amazonian Prime</span>
+    </div>
+  )
+  
+  if (!user) {
+    return null
+  }
+
   const { address } = user;
   return (
     <div className="banner">
