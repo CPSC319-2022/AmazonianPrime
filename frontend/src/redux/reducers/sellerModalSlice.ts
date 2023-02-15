a6cb9e3541a95228af63741a13e43ed8bc00d5bf
William Suryawidjaja <rafa3l389@gmail.com>
2023-02-14 17:26:37 -0800

    feat: Seller Register Modal Form (#23)
    * feat: created listingModal component
    
    * feat: added seller registration modal
    
    * chore: ran yarn verify
    
    * feat: Added seller modal state to redux store and css chores
    
    * chore: Ran yarn verify

---
+++ frontend/src/redux/reducers/sellerModalSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export interface SellerModalState {
+  isSellerModalOpen: boolean;
+}
+
+const initialState: SellerModalState = {
+  isSellerModalOpen: false,
+};
+
+export const sellerModalSlice = createSlice({
+  name: 'sellerModal',
+  initialState,
+  reducers: {
+    toggleModal: (state, action: PayloadAction<boolean>) => {
+      state.isSellerModalOpen = action.payload;
+    },
+  },
+});
+
+export const { toggleModal } = sellerModalSlice.actions;
+export default sellerModalSlice.reducer;
