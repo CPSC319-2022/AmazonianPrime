2a352ab68825bb0278f79e4d5bbb525a7c6c9256
Michael He <michaelhe@Michaels-MacBook-Pro.local>
2023-02-15 12:58:06 -0800

    feat(BuyerRegistration): text fields and button working

--- frontend/src/components/login/BuyerRegistration.tsx
+++ frontend/src/components/login/BuyerRegistration.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import './BuyerRegistration.scss';
 import Banner from '../common/Banner';
-
 import { useNavigate } from 'react-router-dom';
-
-import { Button } from '@mui/material';
+import {
+  Button,
+  TextField,
+  Grid,
+} from '@mui/material';
+import TrendingFlatIcon from '@mui/icons-material/TrendingFlat';
 
 function BuyerRegistration() {
     const navigate = useNavigate();
-    function openRegistrationPage() {
-        navigate(`/login/signup`);
+    function openHomePage() {
+        navigate(`/`);
     }
   return (
     <div className="buyer-registration-page">
@@ -27,7 +30,30 @@ function BuyerRegistration() {
             fellow Amazonians, <br />please give us some more information about yourself.
             </span>
           </div>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <TextField fullWidth required label="First Name" defaultValue="" variant="filled" />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField fullWidth required label="Last Name" defaultValue="" variant="filled" />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField fullWidth required label="User Name" defaultValue="" variant="filled" />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField fullWidth required label="Department" defaultValue="" variant="filled" />
+            </Grid>
+          </Grid>
         </div>
+        <Button
+          color="secondary"
+          variant="contained"
+          className="buyer-registration-page__button"
+          endIcon={<TrendingFlatIcon />}
+          onClick={() => openHomePage()}
+          >
+          Start Shopping
+        </Button>
       </div>
     </div>
   );
