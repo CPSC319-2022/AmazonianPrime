baa0d91863a930e4ca6b32b68f0ae406f5b5bc6b
EllynChan <64517569+EllynChan@users.noreply.github.com>
2023-02-22 11:51:29 -0800

    yarn verified

--- frontend/src/components/product-details-page/Details.tsx
+++ frontend/src/components/product-details-page/Details.tsx
@@ -6,37 +6,29 @@ function Details() {
   if (!listing) {
     return null;
   }
-  const getDetailsRow = (category: string ,value: string) => (
+  const getDetailsRow = (category: string, value: string) => (
     <>
-    <Grid item xs={3}>
+      <Grid item xs={3}>
         <p className="grey">{category}</p>
-    </Grid>
-    <Grid item xs={9}>
-        <p  className="category-value" >{value}</p>
-    </Grid>
+      </Grid>
+      <Grid item xs={9}>
+        <p className="category-value">{value}</p>
+      </Grid>
     </>
-  )
+  );
   return (
     <div>
       <div className="small-header">Details</div>
       <Grid container rowSpacing={0} className="small-font">
-        {
-          getDetailsRow("Condition", listing.condition)
-        }
-        {
-          getDetailsRow("Size", "N/A")
-        }
-        {
-          getDetailsRow("Brand", "N/A")
-        }
-        {
-          getDetailsRow("Colour", "N/A")
-        }
+        {getDetailsRow('Condition', listing.condition)}
+        {getDetailsRow('Size', 'N/A')}
+        {getDetailsRow('Brand', 'N/A')}
+        {getDetailsRow('Colour', 'N/A')}
         <Grid item xs={3}>
           <p className="grey">Quantity</p>
         </Grid>
         <Grid item xs={9} marginTop={1}>
-          <Select className="select" defaultValue={1} size="small" style={{backgroundColor: '#e0e0e0'}}>
+          <Select className="select" defaultValue={1} size="small" style={{ backgroundColor: '#e0e0e0' }}>
             <MenuItem value="1">1</MenuItem>
             <MenuItem value="2">2</MenuItem>
           </Select>
@@ -45,7 +37,5 @@ function Details() {
     </div>
   );
 }
-
-
 
 export default Details;
--- frontend/src/components/product-details-page/ProductDetails.tsx
+++ frontend/src/components/product-details-page/ProductDetails.tsx
@@ -5,34 +5,32 @@ import { Button, Grid } from '@mui/material';
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 import Details from './Details';
 
-function ProductDetails () {
+function ProductDetails() {
   const listing = useAppSelector((state) => state.listings.listingDetails);
   if (!listing) {
     return null;
   }
-  const { listingName, cost, user} = listing;
+  const { listingName, cost, user } = listing;
 
   return (
     <div className="product-details">
       <h1 className="product-name">{listingName}</h1>
       <Grid container rowSpacing={0}>
         <Grid item xs={0.9} marginTop={0.2} marginBottom={0.2}>
-          <div className="circle">{user.firstName.charAt(0)}{user.lastName.charAt(0)}</div>
+          <div className="circle">
+            {user.firstName.charAt(0)}
+            {user.lastName.charAt(0)}
+          </div>
         </Grid>
         <Grid item xs={11.1} marginTop={0.5}>
           <div className="user-name">
-            {user.firstName}&nbsp;{user.lastName.charAt(0)}.
-            <div className="user-department">
-              Marketing
-            </div>
-          </div> 
+            {user.firstName}&nbsp;{user.lastName.charAt(0)}.<div className="user-department">Marketing</div>
+          </div>
         </Grid>
       </Grid>
       <Grid container rowSpacing={0}>
         <Grid item xs={1.5}>
-          <p className="product-cost">
-            C${cost}
-          </p>
+          <p className="product-cost">C${cost}</p>
         </Grid>
         <Grid item xs={10.5}>
           <p className="listed-time">Listed 1 hour ago</p>
@@ -46,22 +44,29 @@ function ProductDetails () {
           <p className="small-font">Offers shipping to desk and home</p>
         </Grid>
         <div className="buttons">
-          <Button variant="contained" color="secondary" sx={{paddingBottom: 0, paddingTop: 0, minWidth: 120, boxShadow: 0}}>
+          <Button
+            variant="contained"
+            color="secondary"
+            sx={{ paddingBottom: 0, paddingTop: 0, minWidth: 120, boxShadow: 0 }}
+          >
             Add to Cart
           </Button>
-          <Button variant="contained" color="secondary" sx={{ml: 2, paddingBottom: 0, paddingTop: 0, minWidth: 120, boxShadow: 0}}>
+          <Button
+            variant="contained"
+            color="secondary"
+            sx={{ ml: 2, paddingBottom: 0, paddingTop: 0, minWidth: 120, boxShadow: 0 }}
+          >
             Buy Now
           </Button>
         </div>
       </Grid>
- <Details />
+      <Details />
       <div>
         <div className="small-header">Description</div>
         <div className="description">{listing.description}</div>
       </div>
     </div>
   );
-
 }
 
 export default ProductDetails;
