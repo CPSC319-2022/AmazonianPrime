7bd1d413197f906dfa9c735fa29c5e93f9b9d128
mahmoudalkhatib <mahmoudalkhatib.ubc@gmail.com>
2023-03-07 02:32:11 -0800

    - Function now processes list of base64 images - use uuid instead of time stamps for keys -

--- backend/functions/upload-function/app.js
+++ backend/functions/upload-function/app.js
@@ -33,36 +33,3 @@ exports.lambdaHandler = async (event, context) => {
         body: JSON.stringify(results),
     };
 };
-
-
-// exports.lambdaHandler = async (event, context) => {
-//     // get the image body from the event
-//     const imageBody = JSON.parse(event.body).base64;
-//     if (!imageBody) {
-//         return {
-//             statusCode: 400,
-//             body: JSON.stringify({error: 'body is null '}),
-//         };
-//     }
-//     // parse the image body into a buffer
-//     const imageBuffer = Buffer.from(imageBody, 'base64');
-//     console.log(imageBuffer);
-//     // generate a unique key for the image based on the current timestamp
-//     const key = `${uuid()}.jpeg`;
-//
-//     // write the image buffer into an S3 bucket with the generated key
-//     const params = {
-//         Bucket: bucketName,
-//         Key: key,
-//         Body: imageBuffer
-//     };
-//
-//     const result = await s3.upload(params).promise();
-//
-//     // return a success message with the generated key
-//     return {
-//         statusCode: 200,
-//         body: JSON.stringify(result)
-//     };
-//     // return result;
-// };
