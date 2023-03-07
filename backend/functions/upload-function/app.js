dba436d14ac885eddf6e07af074ca67c4ad54155
mahmoudalkhatib <mahmoudalkhatib.ubc@gmail.com>
2023-03-07 02:31:16 -0800

    - Function now processes list of base64 images - use uuid instead of time stamps for keys -

--- backend/functions/upload-function/app.js
+++ backend/functions/upload-function/app.js
@@ -1,35 +1,68 @@
 const AWS = require('aws-sdk');
+const {v4: uuid} = require('uuid');
 const s3 = new AWS.S3();
 const bucketName = process.env.S3_BUCKET_NAME;
 
+
 exports.lambdaHandler = async (event, context) => {
-    // get the image body from the event
-    const imageBody = JSON.parse(event.body).base64;
-    if (!imageBody) {
+
+    const listOfImages = JSON.parse(event.body).images;
+    if (!Array.isArray(listOfImages) || listOfImages.length === 0) {
         return {
             statusCode: 400,
-            body: JSON.stringify({error: 'body is null '}),
+            body: JSON.stringify({error: 'Invalid image list'}),
         };
     }
-    // parse the image body into a buffer
-    const imageBuffer = Buffer.from(imageBody, 'base64');
-    console.log(imageBuffer);
-    // generate a unique key for the image based on the current timestamp
-    const key = `images/${Date.now()}.jpeg`;
 
-    // write the image buffer into an S3 bucket with the generated key
-    const params = {
-        Bucket: bucketName,
-        Key: key,
-        Body: imageBuffer
-    };
+    const results = [];
+    for (const image of listOfImages) {
+        const imageBuffer = Buffer.from(image, 'base64');
+        const key = `${uuid()}.jpeg`;
+        const params = {
+            Bucket: bucketName,
+            Key: key,
+            Body: imageBuffer,
+        };
 
-    const result = await s3.upload(params).promise();
+        const result = await s3.upload(params).promise();
+        results.push(result);
+    }
 
-    // return a success message with the generated key
     return {
         statusCode: 200,
-        body: JSON.stringify(result)
+        body: JSON.stringify(results),
     };
-    // return result;
 };
+
+
+// exports.lambdaHandler = async (event, context) => {
+//     // get the image body from the event
+//     const imageBody = JSON.parse(event.body).base64;
+//     if (!imageBody) {
+//         return {
+//             statusCode: 400,
+//             body: JSON.stringify({error: 'body is null '}),
+//         };
+//     }
+//     // parse the image body into a buffer
+//     const imageBuffer = Buffer.from(imageBody, 'base64');
+//     console.log(imageBuffer);
+//     // generate a unique key for the image based on the current timestamp
+//     const key = `${uuid()}.jpeg`;
+//
+//     // write the image buffer into an S3 bucket with the generated key
+//     const params = {
+//         Bucket: bucketName,
+//         Key: key,
+//         Body: imageBuffer
+//     };
+//
+//     const result = await s3.upload(params).promise();
+//
+//     // return a success message with the generated key
+//     return {
+//         statusCode: 200,
+//         body: JSON.stringify(result)
+//     };
+//     // return result;
+// };
