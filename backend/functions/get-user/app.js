50f88e1fc1d640cedb4a528b506ad8ff5ca29dd7
Michael He <michaelhe@Michaels-MBP.hitronhub.home>
2023-03-11 15:41:14 -0800

    merge, also changed parseJWT

--- backend/functions/get-user/app.js
+++ backend/functions/get-user/app.js
@@ -3,7 +3,13 @@ var OAuth2Client = require("google-auth-library");
 const dbConnection = require("dbConnection.js");
 
 function parseJWT (token) {
-  return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+
+  return JSON.parse(jsonPayload);
 }
 
 /**
