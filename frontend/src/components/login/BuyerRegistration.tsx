a283dda03e7118c2880d2a9114eea0cb7596f8ee
Michael He <michaelhe@Michaels-MBP.hitronhub.home>
2023-03-13 02:06:38 -0700

    test using UpdateUser

--- frontend/src/components/login/BuyerRegistration.tsx
+++ frontend/src/components/login/BuyerRegistration.tsx
@@ -30,7 +30,7 @@ function BuyerRegistration() {
 
   function register() {
     updateProfile(updatedUser);
-    dispatch(setUser({ Department: 'AWS' }));
+    dispatch(setUser(result.data));
   }
   function handleShippingCheckbox() {
     setUseBillingAddressForShipping(!useBillingAddressForShipping);
