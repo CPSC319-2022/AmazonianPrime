dc9d68b04d931e0b38d8f2527ebfc21e47c22675
EllynChan <64517569+EllynChan@users.noreply.github.com>
2023-03-14 10:55:19 -0700

    changed: db-init, add banking to updated version

--- backend/functions/add-banking/app.js
+++ backend/functions/add-banking/app.js
@@ -19,11 +19,12 @@ exports.lambdaHandler = async (event, context) => {
     "databaseAmazonianPrime"
   );
 
-  const { UserID, InstitutionNum, AccountNum, TransitNum } =
+  const { UserID, AddressID, InstitutionNum, AccountNum, TransitNum } =
     JSON.parse(event.body);
 
   if (
       !UserID ||
+      !AddressID ||
       !InstitutionNum ||
       !AccountNum ||
       !TransitNum
@@ -34,7 +35,7 @@ exports.lambdaHandler = async (event, context) => {
       };
     }
 
-  const addBankingDetailsQuery = `INSERT INTO BankingDetails(InstitutionNum, AccountNum, TransitNum) VALUES(${InstitutionNum}, ${AccountNum}, ${TransitNum})`;
+  const addBankingDetailsQuery = `INSERT INTO BankingDetails(UserID, AddressID, InstitutionNum, AccountNum, TransitNum) VALUES(${UserID}, ${AddressID}, ${InstitutionNum}, ${AccountNum}, ${TransitNum})`;
 
   const addBankingDetails = await new Promise((resolve, reject) => {
     con.query(addBankingDetailsQuery, function (err, res) {
@@ -46,17 +47,6 @@ exports.lambdaHandler = async (event, context) => {
   });
 
   const BankingID = addBankingDetails["insertId"];
-
-  const addBankingAccountQuery = `INSERT INTO BankingAccount(BankingID, UserID) VALUES(${BankingID}, ${UserID})`;
-
-  const addBankingAccount = await new Promise((resolve, reject) => {
-    con.query(addBankingAccountQuery, function (err, res) {
-      if (err) {
-        reject("Couldn't add the banking account to database!");
-      }
-      resolve(res);
-    });
-  });
 
   const getBankingByIdQuery = `SELECT * FROM BankingDetails WHERE BankingID = "${BankingID}"`;
 
