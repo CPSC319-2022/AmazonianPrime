1f84de1d720beed419d22cffdd0c77e3a6bc3ba2
mahmoudalkhatib <mahmoudalkhatib.ubc@gmail.com>
2023-03-14 22:35:12 -0700

    Added optional params and changed the createListing Query.

--- backend/functions/create-listing/app.js
+++ backend/functions/create-listing/app.js
@@ -1,23 +1,16 @@
-var { v4: uuidv4 } = require("uuid");
-const dbConnection = require("dbConnection.js");
-var mysql = require("mysql");
+const AWS = require('aws-sdk');
+const {v4: uuid} = require('uuid');
+const dbConnection = require('dbConnection.js');
+const s3 = new AWS.S3();
+const bucketName = process.env.S3_BUCKET_NAME;
 
-/**
- * Sample Lambda function which mocks the operation of buying a random number of shares for a stock.
- * For demonstration purposes, this Lambda function does not actually perform any  actual transactions. It simply returns a mocked result.
- *
- * @param {Object} event - Input event to the Lambda function
- * @param {Object} context - Lambda Context runtime methods and attributes
- *
- * @returns {Object} object - Object containing details of the stock buying transaction
- *
- */
 exports.lambdaHandler = async (event, context) => {
+
   const con = await dbConnection.connectDB(
-    process.env.DatabaseAddress,
-    "user",
-    "Password1234",
-    "databaseAmazonianPrime"
+      process.env.DatabaseAddress,
+      'user',
+      'Password1234',
+      'databaseAmazonianPrime',
   );
 
   const {
@@ -31,23 +24,25 @@ exports.lambdaHandler = async (event, context) => {
     Brand,
     Colour,
     ItemCondition,
+    Images,
   } = JSON.parse(event.body);
 
   if (
-    !UserID ||
-    !ListingName ||
-    !Description ||
-    !Cost ||
-    !Quantity ||
-    !Category ||
-    !ItemCondition
+      !UserID ||
+      !ListingName ||
+      !Description ||
+      !Cost ||
+      !Quantity ||
+      !Category ||
+      !ItemCondition ||
+      !Array.isArray(Images) ||
+      Images.length === 0
   ) {
     return {
       statusCode: 400,
-      body: "Missing required fields",
+      body: 'Missing required fields',
     };
   }
-
   const createListingQuery = `INSERT INTO Listing(UserID, ListingName, Description, Cost, Quantity, Category, ${
       Size !== undefined ? 'Size, ' : ''
   }${Brand !== undefined ? 'Brand, ' : ''}${
@@ -58,37 +53,106 @@ exports.lambdaHandler = async (event, context) => {
       Colour !== undefined ? `"${Colour}",` : ''
   } "${ItemCondition}", true)`;
 
+
+  let createListing;
   try {
-    const createListing = await new Promise((resolve, reject) => {
+    createListing = await new Promise((resolve, reject) => {
       con.query(createListingQuery, function (err, res) {
         if (err) {
-          reject("Couldn't add listing to database!");
+          reject(err);
         }
         resolve(res);
       });
     });
-
-    const listingID = createListing["insertId"];
-
-    const getListingByIdQuery = `SELECT * FROM Listing WHERE ListingID = "${listingID}"`;
-
-    const getListing = await new Promise((resolve, reject) => {
-      con.query(getListingByIdQuery, function (err, res) {
-        if (err) {
-          reject("Couldn't get the listing from database!");
-        }
-        resolve(res);
-      });
-    });
-
+  } catch(err){
     return {
-      statusCode: 200,
-      body: JSON.stringify(getListing[0]),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify(error),
-    };
+      statusCode: 400,
+      body: JSON.stringify({error: err})
+    }
   }
+
+  const ListingID = createListing['insertId'];
+  console.log("ListingID: " + ListingID)
+
+  const getListingByIdQuery = `SELECT * FROM Listing WHERE ListingID = "${ListingID}"`;
+
+  const getListing = await new Promise((resolve, reject) => {
+    con.query(getListingByIdQuery, function (err, res) {
+      if (err) {
+        console.log("Failed on line 93")
+        return {
+          status:400,
+          body: JSON.stringify(err)
+        }
+      }
+      resolve(res);
+    });
+  });
+
+  for (const image of Images) {
+    let imageBuffer;
+    try {
+      imageBuffer = Buffer.from(image, 'base64');
+    } catch (error) {
+      console.log("Failed on line 109")
+      return {
+        statusCode: 400,
+        body: JSON.stringify({error: error})
+      }
+    }
+
+    const key = `${uuid()}.jpeg`;
+    const params = {
+      Bucket: bucketName,
+      Key: key,
+      Body: imageBuffer,
+    };
+    let result;
+    try {
+      result = await s3.upload(params).promise();
+      console.log('Upload successful: ', result);
+    } catch (err) {
+      console.log("Failed on line 126")
+      return{
+        statusCode: 400,
+        body: JSON.stringify(err)
+      }
+    }
+
+
+    const addListingQuery = `INSERT INTO ListingImage (ListingID, S3ImagePath)
+                                 VALUES (?, ?);`;
+    console.log("adding to DB ")
+    const addImage = await new Promise((resolve, reject) => {
+      con.query(addListingQuery, [ListingID, result.Location], function (err, res) {
+        if (err) {
+          console.log("Failed on line 140")
+          reject(err);
+        }
+        resolve(res);
+      });
+    });
+
+    const PictureID = addImage['insertId'];
+    const getListingImageByIdQuery = `SELECT * FROM ListingImage WHERE PictureID = ${PictureID}`;
+    const getListing = await new Promise((resolve, reject) => {
+      con.query(getListingImageByIdQuery, function (err, res) {
+        if (err) {
+          console.log("Failed on line 153")
+          reject(err)
+          return {
+            status:400,
+            body: JSON.stringify(err)
+          }
+        }
+        resolve(res);
+      });
+    });
+
+
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({message: "Addition was succesful"}),
+  };
 };
