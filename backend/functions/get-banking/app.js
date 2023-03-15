58713d4cd260081dc54b5a23071e1d4ae8933fd8
EllynChan <64517569+EllynChan@users.noreply.github.com>
2023-03-14 20:06:28 -0700

    added template yaml arguments not tested

--- backend/functions/get-banking/app.js
+++ backend/functions/get-banking/app.js
@@ -1,10 +1,6 @@
 const mysql = require('mysql');
 const dbConnection = require('dbConnection.js');
 
-function parseJWT(token) {
-  return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
-}
-
 /**
  * Sample Lambda function which mocks the operation of buying a random number of shares for a stock.
  * For demonstration purposes, this Lambda function does not actually perform any  actual transactions. It simply returns a mocked result.
