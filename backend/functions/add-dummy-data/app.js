58ddfe40215bc537f753ae1aa0823377b6b0734b
EllynChan <64517569+EllynChan@users.noreply.github.com>
2023-03-16 23:19:14 -0700

    changing error output, fixed bugs

--- backend/functions/add-dummy-data/app.js
+++ backend/functions/add-dummy-data/app.js
@@ -22,14 +22,15 @@ exports.lambdaHandler = async (event, context) => {
   const addUserQueries = [
     `INSERT INTO Users (UserID, FirstName, LastName, Email, Department, IsAdmin) VALUES (1, "John", "Doe", "johndoe@gmail.com", "Marketing", false)`,
     `INSERT INTO Users (UserID, FirstName, LastName, Email, Department, IsAdmin) VALUES (2, "Alice", "Ather", "aather@gmail.com", null, false)`,
-    `INSERT INTO Users (UserID, FirstName, LastName, Email, Department, IsAdmin) VALUES (3, "Bob", "Carlson", "bobc321@gmail.com", "Management", true)`,
+    `INSERT INTO Users (UserID, FirstName, LastName, Email, Department, IsAdmin) VALUES (3, "Bob", "Carlson", "bobc321@gmail.com", "Management", true)`
   ];
 
   addUserQueries.forEach(async (queryString) => {
     let insertUsers = await new Promise((resolve, reject) => {
       con.query(queryString, function (err, res) {
         if (err) {
-          reject('Error inserting dummy data into Users');
+          console.log('Error inserting dummy data into Users');
+          reject(err);
         }
         resolve(res);
       });
@@ -37,16 +38,17 @@ exports.lambdaHandler = async (event, context) => {
   });
 
   const addListingsQueries = [
-    `INSERT INTO Listing (ListingID, UserID, ListingName, Description, Cost, Quantity, Category, ItemCondition, PostedTimestamp, IsActiveListing) VALUES (1, 1, "MCM Arm Chair", "Great condition.", 40.00, 2, "Amazon Merchandise", "Used- Like New", "2023-03-01 12:00:00", true)`,
-    `INSERT INTO Listing (ListingID, UserID, ListingName, Description, Cost, Quantity, Category, ItemCondition, PostedTimestamp, IsActiveListing) VALUES (2, 2, "Healthy Monstera", "Selling this very healthy monstera", 20.00, 1, "Garden & Outdoors", "New", "2023-03-01 12:00:00", true)`,
-    `INSERT INTO Listing (ListingID, UserID, ListingName, Description, Cost, Quantity, Category, ItemCondition, PostedTimestamp, IsActiveListing) VALUES (3, 3, "Listing 3", "Listing 3 Description", 5.00, 10, "Miscellaneous", "Used Fair", "2023-03-10 12:00:00", false)`,
+    `INSERT INTO Listing (ListingID, UserID, ListingName, Description, Cost, Quantity, Category, Size, Brand, Colour, ItemCondition, PostedTimestamp, IsActiveListing) VALUES (1, 1, "MCM Arm Chair", "Great condition.", 40.00, 2, "Amazon Merchandise", "1m", "Unknown", "Brown", "Used- Like New", "2023-03-01 12:00:00", true)`,
+    `INSERT INTO Listing (ListingID, UserID, ListingName, Description, Cost, Quantity, Category, Size, Brand, Colour, ItemCondition, PostedTimestamp, IsActiveListing) VALUES (2, 2, "Healthy Monstera", "Selling this very healthy monstera", 20.00, 1, "Garden & Outdoors", "2m", NULL, NULL, "New", "2023-03-01 12:00:00", true)`,
+    `INSERT INTO Listing (ListingID, UserID, ListingName, Description, Cost, Quantity, Category, Size, Brand, Colour, ItemCondition, PostedTimestamp, IsActiveListing) VALUES (3, 3, "Listing 3", "Listing 3 Description", 5.00, 10, "Miscellaneous", NULL, NULL, NULL, "Used Fair", "2023-03-10 12:00:00", false)`
   ];
 
   addListingsQueries.forEach(async (queryString) => {
     let insertListing = await new Promise((resolve, reject) => {
       con.query(queryString, function (err, res) {
         if (err) {
-          reject('Error inserting dummy data into Listings');
+          console.log('Error inserting dummy data into Listings');
+          reject(err);
         }
         resolve(res);
       });
@@ -56,14 +58,15 @@ exports.lambdaHandler = async (event, context) => {
   const addCountryQueries = [
     `INSERT INTO Country (CityName, Province, StreetAddress, PostalCode, Country) VALUES ("Vanvouver", "BC", "100 St", "V0V 0V0", "Canada")`,
     `INSERT INTO Country (CityName, Province, StreetAddress, PostalCode, Country) VALUES ("Richmond", "BC", "1000 St", "V1V 1V1", "Canada")`,
-    `INSERT INTO Country (CityName, Province, StreetAddress, PostalCode, Country) VALUES ("Toronto", "ON", "2000 St", "V2V 2V2", "Canada")`,
+    `INSERT INTO Country (CityName, Province, StreetAddress, PostalCode, Country) VALUES ("Toronto", "ON", "2000 St", "V2V 2V2", "Canada")`
   ];
 
   addCountryQueries.forEach(async (queryString) => {
     let insertCountry = await new Promise((resolve, reject) => {
       con.query(queryString, function (err, res) {
         if (err) {
-          reject('Error inserting dummy data into Country');
+          console.log('Error inserting dummy data into Country');
+          reject(err);
         }
         resolve(res);
       });
@@ -73,14 +76,15 @@ exports.lambdaHandler = async (event, context) => {
   const addAddressQueries = [
     `INSERT INTO Address (AddressID, CityName, Province, StreetAddress) VALUES (1, "Vanvouver", "BC", "100 St")`,
     `INSERT INTO Address (AddressID, CityName, Province, StreetAddress) VALUES (2, "Richmond", "BC", "1000 St")`,
-    `INSERT INTO Address (AddressID, CityName, Province, StreetAddress) VALUES (3, "Toronto", "ON", "2000 St")`,
+    `INSERT INTO Address (AddressID, CityName, Province, StreetAddress) VALUES (3, "Toronto", "ON", "2000 St")`
   ];
 
   addAddressQueries.forEach(async (queryString) => {
     let insertAddress = await new Promise((resolve, reject) => {
       con.query(queryString, function (err, res) {
         if (err) {
-          reject('Error inserting dummy data into Address');
+          console.log('Error inserting dummy data into Address');
+          reject(err);
         }
         resolve(res);
       });
@@ -90,14 +94,15 @@ exports.lambdaHandler = async (event, context) => {
   const addPaymentDetailsQueries = [
     `INSERT INTO PaymentDetails (PaymentID, UserID, AddressID, CreditCardNum, ExpiryDate, CVV, CardHolderName) VALUES (1, 1, 1, 1234123412341234, "1025", 100, "John Doe")`,
     `INSERT INTO PaymentDetails (PaymentID, UserID, AddressID, CreditCardNum, ExpiryDate, CVV, CardHolderName) VALUES (2, 2, 2, 4321432143214321, "0626", 200, "Alice Ather")`,
-    `INSERT INTO PaymentDetails (PaymentID, UserID, AddressID, CreditCardNum, ExpiryDate, CVV, CardHolderName) VALUES (3, 3, 3, 1234567812345678, "0825", 300, "Bob Carlson")`,
+    `INSERT INTO PaymentDetails (PaymentID, UserID, AddressID, CreditCardNum, ExpiryDate, CVV, CardHolderName) VALUES (3, 3, 3, 1234567812345678, "0825", 300, "Bob Carlson")`
   ];
 
   addPaymentDetailsQueries.forEach(async (queryString) => {
     let insertPaymentDetails = await new Promise((resolve, reject) => {
       con.query(queryString, function (err, res) {
         if (err) {
-          reject('Error inserting dummy data into PaymentDetails');
+          console.log('Error inserting dummy data into PaymentDetails');
+          reject(err);
         }
         resolve(res);
       });
@@ -106,15 +111,16 @@ exports.lambdaHandler = async (event, context) => {
 
   const addBankingDetailsQueries = [
     `INSERT INTO BankingDetails (BankingID, UserID, AddressID, InstitutionNum, AccountNum, TransitNum, NameOnCard) VALUES (1, 1, 1, 001, 001, 001, "John Doe")`,
-    `INSERT INTO PaymentDetails (PaymentID, UserID, AddressID, CreditCardNum, ExpiryDate, CVV, CardHolderName) VALUES (2, 2, 2, 002, 002, 002, "Alice Ather")`,
-    `INSERT INTO PaymentDetails (PaymentID, UserID, AddressID, CreditCardNum, ExpiryDate, CVV, CardHolderName) VALUES (3, 3, 3, 003, 003, 003, "Bob Carlson")`,
+    `INSERT INTO BankingDetails (BankingID, UserID, AddressID, InstitutionNum, AccountNum, TransitNum, NameOnCard) VALUES (2, 2, 2, 002, 002, 002, "Alice Ather")`,
+    `INSERT INTO BankingDetails (BankingID, UserID, AddressID, InstitutionNum, AccountNum, TransitNum, NameOnCard) VALUES (3, 3, 3, 003, 003, 003, "Bob Carlson")`
   ];
 
   addBankingDetailsQueries.forEach(async (queryString) => {
     let insertBankingDetails = await new Promise((resolve, reject) => {
       con.query(queryString, function (err, res) {
         if (err) {
-          reject('Error inserting dummy data into BankingDetails');
+          console.log('Error inserting dummy data into BankingDetails');
+          reject(err);
         }
         resolve(res);
       });
@@ -126,7 +132,8 @@ exports.lambdaHandler = async (event, context) => {
   const getAllUsers = await new Promise((resolve, reject) => {
     con.query(getAllUsersQuery, function (err, res) {
       if (err) {
-        reject("Couldn't get all Users from database!");
+        console.log("Couldn't get all Users from database!");
+        reject(err);
       }
       resolve(res);
     });
