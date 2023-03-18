cee028c94ce0008a16a1cb0684bf254d12d932ea
Tristan Martinuson <tmartinuson@gmail.com>
2023-03-18 16:14:49 -0700

    removed duplicate create listing in template yaml, added new format return for get shopping cart
    New response value as detailed:
    {
      "TotalQuantity": 3,
      "Items": [
        {
          "ShoppingCartItemID": 2,
          "UserID": 1,
          "ListingID": 7,
          "Quantity": 1,
          "Listing": {
            "ListingID": 7,
            "UserID": 2,
            "ListingName": "Chess",
            "Description": "Board game",
            "Cost": 5,
            "Quantity": 2,
            "Category": "toy",
            "Size": null,
            "Brand": null,
            "Colour": null,
            "ItemCondition": "used",
            "PostedTimestamp": "2023-03-16T08:14:24.000Z",
            "IsActiveListing": 1,
            "ImageListingID": null,
            "IsAdmin": 0,
            "User": {
              "FirstName": "Sally",
              "LastName": "Dane",
              "Email": "sa@gmail.com",
              "Department": "AWS"
            },
            "ImagePreview": null
          }
        },
        {
          "ShoppingCartItemID": 5,
          "UserID": 1,
          "ListingID": 6,
          "Quantity": 2,
          "Listing": {
            "ListingID": 6,
            "UserID": 1,
            "ListingName": "Cards",
            "Description": "Playing cards",
            "Cost": 15,
            "Quantity": 3,
            "Category": "toy",
            "Size": null,
            "Brand": null,
            "Colour": null,
            "ItemCondition": "used",
            "PostedTimestamp": "2023-03-16T08:12:47.000Z",
            "IsActiveListing": 1,
            "ImageListingID": null,
            "IsAdmin": 0,
            "User": {
              "FirstName": "John",
              "LastName": "Meyer",
              "Email": "johnmeyer@gmail.com",
              "Department": "AWS"
            },
            "ImagePreview": null
          }
        }
      ]
    }

--- backend/functions/get-cart/app.js
+++ backend/functions/get-cart/app.js
@@ -18,12 +18,14 @@ exports.lambdaHandler = async (event, context) => {
     'Password1234',
     'databaseAmazonianPrime',
   );
-
   const UserID = event.pathParameters.userid;
+  const Response = {};
+  Response.TotalQuantity = 0;
   
-  const getCartQuery = `SELECT * FROM Listing WHERE ListingID IN (SELECT ListingID FROM ShoppingCartItem WHERE UserID = ${UserID})`;
+  //TODO if we have time, flesh out a giant join and transform to fit the expected result, reduce MySQL call to 1
+  const getCartQuery = `SELECT * FROM ShoppingCartItem WHERE UserID = ${UserID}`;
 
-  const getShoppingCart = await new Promise((resolve, reject) => {
+  const Items = await new Promise((resolve, reject) => {
     con.query(getCartQuery, function (err, res) {
       if (err) {
         reject(err);
@@ -32,10 +34,32 @@ exports.lambdaHandler = async (event, context) => {
     });
   });
 
-  console.log(getShoppingCart);
-
+  for (const shoppingCartItem of Items) {
+    const getListingQuery = `SELECT * FROM Listing LEFT JOIN (SELECT ListingID AS ImageListingID, S3ImagePath FROM ListingImage WHERE S3ImagePath IN (SELECT MAX(S3ImagePath) FROM ListingImage GROUP BY ListingID)) AS Images ON Listing.ListingID = Images.ImageListingID JOIN Users on Listing.UserID = Users.UserID WHERE Listing.ListingID = ${shoppingCartItem.ListingID};`;
+    const Listing = await new Promise((resolve, reject) => {
+    con.query(getListingQuery, function (err, res) {
+        if (err) {
+          reject(err);
+        }
+        resolve(res);
+      });
+    });
+    const { FirstName, LastName, Email, Department, S3ImagePath, ...ListingData } =
+    Listing[0];
+    shoppingCartItem.Listing = ListingData;
+    shoppingCartItem.Listing.User = {
+      "FirstName": FirstName,
+      "LastName": LastName,
+      "Email": Email,
+      "Department": Department,
+    }
+    shoppingCartItem.Listing.ImagePreview = S3ImagePath;
+    Response.TotalQuantity += shoppingCartItem.Quantity;
+  }
+  Response.Items = Items;
+  console.log(Response);
   return {
     statusCode: 200,
-    body: JSON.stringify(getShoppingCart),
+    body: JSON.stringify(Response),
   };
 };
