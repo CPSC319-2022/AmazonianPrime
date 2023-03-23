0550f25313f47c579e29d240885e754a68cd5d61
Tristan Martinuson <tmartinuson@gmail.com>
2023-03-22 18:56:02 -0700

    fixed get users to have "admin authentication"

--- backend/functions/admin-get-users/app.js
+++ backend/functions/admin-get-users/app.js
@@ -18,10 +18,38 @@ exports.lambdaHandler = async (event, context) => {
     'Password1234',
     'databaseAmazonianPrime',
   );
-  // TODO add admin authentication from context
+
   const offset = event.queryStringParameters.offset;
   const limit = event.queryStringParameters.limit;
-  var getUsersQuery = `SELECT * FROM Users LIMIT ${limit} OFFSET ${offset}`;
+
+  const { UserID } = JSON.parse(event.body);
+
+  if (UserID === null || UserID == undefined) {
+    return {
+      statusCode: 400,
+      body: `Missing Admin UserID. Please provide a Admin UserID in the Request Body.`,
+    };
+  }
+
+  const getAdminQuery = `SELECT IsAdmin FROM Users WHERE UserID = ${UserID}`;
+
+  const getAdmin = await new Promise((resolve, reject) => {
+    con.query(getAdminQuery, function (err, res) {
+      if (err) {
+        reject(err);
+      }
+      resolve(res);
+    });
+  });
+
+  if (getAdmin.length < 1) {
+    return {
+      statusCode: 404,
+      body: `Admin with UserID ${UserID} not found`,
+    };
+  }
+
+  const getUsersQuery = `SELECT * FROM Users LIMIT ${limit} OFFSET ${offset}`;
 
   const Users = await new Promise((resolve, reject) => {
     con.query(getUsersQuery, function (err, res) {
