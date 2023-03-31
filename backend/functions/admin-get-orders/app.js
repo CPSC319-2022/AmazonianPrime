31619f44b307ac80167188ef834d49a5f34d6901
mahmoudalkhatib <mahmoudalkhatib.ubc@gmail.com>
2023-03-31 16:29:20 -0700

    Add more information to get orders

--- backend/functions/admin-get-orders/app.js
+++ backend/functions/admin-get-orders/app.js
@@ -54,6 +54,50 @@ exports.lambdaHandler = async (event, context) => {
     });
   });
 
+  for(const order of Orders) {
+    const getPaymentQuery = `SELECT * FROM PaymentDetails, Address WHERE PaymentDetails.AddressID = Address.AddressID AND PaymentDetails.UserID = ${UserID}`;
+
+    order.Payment = await new Promise((resolve, reject) => {
+      con.query(getPaymentQuery, function (err, res) {
+        if (err) {
+          reject("Couldn't get the payment details from database!");
+        }
+        resolve(res);
+      });
+    });
+    getShippingQuery = `SELECT * FROM ShippingAddress, Address WHERE ShippingAddress.AddressID = Address.AddressID AND ShippingAddress.UserID = order.UserID`;
+
+    order.Shipping = await new Promise((resolve, reject) => {
+      con.query(getPaymentQuery, function (err, res) {
+        if (err) {
+          reject("Couldn't get the payment details from database!");
+        }
+        resolve(res);
+      });
+    });
+    var listings = []
+    for (const shoppingCartItem of Items) {
+      const getListingQuery = `SELECT *
+                               FROM Listing
+                                      LEFT JOIN (SELECT ListingID AS ImageListingID, S3ImagePath
+                                                 FROM ListingImage
+                                                 WHERE S3ImagePath IN
+                                                       (SELECT MAX(S3ImagePath) FROM ListingImage GROUP BY ListingID)) AS Images
+                                                ON Listing.ListingID = Images.ImageListingID
+                                      JOIN Users on Listing.UserID = Users.UserID
+                               WHERE Listing.ListingID = ${shoppingCartItem.ListingID};`;
+      const Listing = await new Promise((resolve, reject) => {
+        con.query(getListingQuery, function (err, res) {
+          if (err) {
+            reject(err);
+          }
+          resolve(res);
+        });
+      });
+      listings.push(Listing);
+    }
+    order.Listings = listings;
+  }
   console.log(Orders);
 
   await dbConnection.disconnectDB(con);
