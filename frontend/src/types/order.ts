c9049e6dd38d603c5c8080d092ece0868849fce8
EllynChan <64517569+EllynChan@users.noreply.github.com>
2023-04-01 16:06:32 -0700

    wip

--- frontend/src/types/order.ts
+++ frontend/src/types/order.ts
@@ -1,14 +1,32 @@
-import { ListingPreview } from './listingPreview';
+import { OrderListings } from "./ordersListing";
 
 export interface Order {
   OrderID: number;
   UserID: number;
-  AddressID: number;
-  PaymentID: number;
   ShippingStatus: string;
   OrderTimestamp: string;
-  PurchaseAmount: null;
-  GSTTax: null;
-  PSTTax: null;
-  TotalAmount: null;
+  Listings: 
+  {
+    OrderID: number;
+    ListingID: number;
+    OrderQuantity: number;
+    UserID: number;
+    ListingName: string;
+    Description: string;
+    Cost: number;
+    Category: string;
+    Size: string;
+    Brand: string;
+    Colour: string;
+    ItemCondition: string;
+    PostedTimestamp: string;
+    IsActiveListing: number;
+    ImageListingID: number;
+    S3ImagePath: string;
+    FirstName: string;
+    LastName: string;
+    Email: string;
+    Department: string;
+    IsAdmin: boolean;
+  }[]
 }
