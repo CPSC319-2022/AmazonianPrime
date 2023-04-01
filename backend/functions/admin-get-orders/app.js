055dc4b876d2a960884d3c86fe37f4697bbf6581
alkhatibmahmoud <59523244+alkhatibmahmoud@users.noreply.github.com>
2023-04-01 12:00:53 -0700

    Add more information to get orders (#143)
    * Add more information to get orders
    
    * fix: fixed admin get orders endpoint
    
    * fix: return order quantity instead of listing
    
    ---------
    
    Co-authored-by: William Suryawidjaja <rafa3l389@gmail.com>

--- backend/functions/admin-get-orders/app.js
+++ backend/functions/admin-get-orders/app.js
@@ -54,10 +54,78 @@ exports.lambdaHandler = async (event, context) => {
     });
   });
 
-  console.log(Orders);
+  for(const order of Orders) {
+    const PaymentID = order['PaymentID']
+    const getPaymentQuery = `SELECT * FROM PaymentDetails, Address WHERE PaymentDetails.AddressID = Address.AddressID AND PaymentDetails.PaymentID = ${PaymentID}`;
+
+    const getPayment = await new Promise((resolve, reject) => {
+      con.query(getPaymentQuery, function (err, res) {
+        if (err) {
+          reject("Couldn't get the payment details from database!");
+        }
+        resolve(res);
+      });
+    });
+    
+    order.Payment = getPayment[0];
+    
+    const AddressID = order['AddressID']
+    
+    const getShippingQuery = `SELECT * FROM Address WHERE Address.AddressID = ${AddressID}`;
+
+    const getShipping = await new Promise((resolve, reject) => {
+      con.query(getShippingQuery, function (err, res) {
+        if (err) {
+          reject("Couldn't get the payment details from database!");
+        }
+        resolve(res);
+      });
+    });
+    
+    order.Shipping = getShipping[0];
+    
+    const GivenOrderID = order['OrderID']
+    
+    const getOrderItemsQuery = `SELECT * FROM OrderItem WHERE OrderItem.OrderID = ${GivenOrderID}`
+    
+    const Items = await new Promise((resolve, reject) => {
+      con.query(getOrderItemsQuery, function (err, res) {
+        if (err) {
+          reject("Couldn't get the payment details from database!");
+        }
+        resolve(res);
+      });
+    });
+    
+    var listings = []
+    
+    for (const orderItem of Items) {
+      const getListingQuery = `SELECT *
+                              FROM Listing
+                                      LEFT JOIN (SELECT ListingID AS ImageListingID, S3ImagePath
+                                                FROM ListingImage
+                                                WHERE S3ImagePath IN
+                                                      (SELECT MAX(S3ImagePath) FROM ListingImage GROUP BY ListingID)) AS Images
+                                                ON Listing.ListingID = Images.ImageListingID
+                                      JOIN Users on Listing.UserID = Users.UserID
+                              WHERE Listing.ListingID = ${orderItem.ListingID};`;
+      const getListing = await new Promise((resolve, reject) => {
+        con.query(getListingQuery, function (err, res) {
+          if (err) {
+            reject(err);
+          }
+          resolve(res);
+        });
+      });
+      const {Quantity, ...rest} = getListing[0];
+      listings.push({...orderItem, ...rest});
+    }
+    
+    order.Listings = listings;
+  }
 
   await dbConnection.disconnectDB(con);
-
+  
   return {
     statusCode: 200,
     body: JSON.stringify(Orders),
