7d1eb882f62f891f1b95a79706fd0840312ac916
alkhatibmahmoud <59523244+alkhatibmahmoud@users.noreply.github.com>
2023-04-01 18:30:10 -0700

    Create a new DB Table For banned users (#135)
    * Create a new DB Table For banned users
    
    * Edit table for blocking IDs

--- backend/functions/db-init/app.js
+++ backend/functions/db-init/app.js
@@ -63,6 +63,23 @@ exports.lambdaHandler = async (event, context) => {
 
   console.log(createTableUsers);
 
+  let createBlockedUsersTableQuery = `CREATE TABLE BlockedUsers (
+                                                   BlockingID int NOT NULL AUTO_INCREMENT,
+                                                   Email varchar(255) UNIQUE NOT NULL,
+                                                   PRIMARY KEY (BlockingID)
+                              );`;
+
+  const createTableBlockedUsers = await new Promise((resolve, reject) => {
+    con.query(createBlockedUsersTableQuery, function (err, res) {
+      if (err) {
+        reject("Couldn't create users table!");
+      }
+      resolve(res);
+    });
+  });
+
+  console.log(createTableBlockedUsers);
+
   let createCountryTableQuery = `CREATE TABLE Country (
     CityName varchar(255) NOT NULL, 
     Province varchar(255) NOT NULL, 
@@ -286,6 +303,8 @@ exports.lambdaHandler = async (event, context) => {
   });
 
   console.log(createShoppingCartItemTable);
+
+
 
   return {
     status: 'SUCCESS',
